3532d4ba147d71675481a8670e93f830224425bf
Wenzhe Liu <liuw888@snorlax.cs.uchicago.edu>
2020-01-16 17:01:18 -0600

    Initial Commit

---
+++ project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by Wenzhe Liu on 1/16/20.
+//  Copyright © 2020 Wenzhe Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
